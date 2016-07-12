4ae199073a0bbb003c06785469d074f851cdb28f
Vincent Alcantara <vincent.alcantara@yahoo.com.au>
2016-07-12 23:28:42 +1000

    Added postcode fields and ajax call

--- src/index.js
+++ src/index.js
@@ -3,38 +3,66 @@ import {render} from 'react-dom';
 
 var AddressForm = React.createClass({
   getInitialState: function() {
-    return {author: '', suburb: ''};
+    return {address: '', suburb: '', postcode: ''};
   },
-  handleAuthorChange: function(e) {
-    this.setState({author: e.target.value});
+  handleAddressChange: function(e) {
+    this.setState({address: e.target.value});
+  },
+  getSuburbs: function(typeahead) {
+  	$.ajax({
+  		type: "GET",
+  		headers: "AUTH-KEY: 872608e3-4530-4c6a-a369-052accb03ca8",
+  		dataType: 'jsonp',
+  		url: "https://digitalapi.auspost.com.au/postcode/search.json?q=" + typeahead,
+  		success: function(response) {
+  			this.handleSuburbChange(response) //
+  		}.bind(this),
+  		error: function(xhr, status, err) {
+    		console.error(this.props.url, status, err.toString());
+  		}.bind(this)
+  	});
   },
   handleSuburbChange: function(e) {
+  	// check if the suburb is at least 3 characters before calling api
+  	// if no match set an error
     this.setState({suburb: e.target.value});
+  },
+  handlePostCode: function(e) {
+  	// check if the postcode is at least 3 characters before calling api
+  	// if no match set an error
+    this.setState({postcode: e.target.value});
   },
   handleSubmit: function(e) {
     e.preventDefault();
-    var author = this.state.author.trim();
+    var address = this.state.address.trim();
     var suburb = this.state.suburb.trim();
-    if (!suburb || !author) {
+    if (!suburb || !address) {
       return;
     }
-    this.props.onAddressSubmit({author: author, suburb: suburb});
-    this.setState({author: '', suburb: ''});
+    this.props.onAddressSubmit({address: address, suburb: suburb});
+    this.setState({address: '', suburb: ''});
   },
   render: function() {
     return (
       <form className="AddressForm" onSubmit={this.handleSubmit}>
+        <div>You've won a prize.  Enter your details to win</div>
         <input
-          type="suburb"
-          placeholder="Your name"
-          value={this.state.author}
-          onChange={this.handleAuthorChange}
+          type="text"
+          placeholder="Enter address"
+          value={this.state.address}
+          onChange={this.handleAddressChange}
         />
         <input
-          type="Suburb"
+          type="text"
           placeholder="Enter suburb"
           value={this.state.suburb}
           onChange={this.handleSuburbChange}
+        />
+         <input
+          type="text"
+          placeholder="Enter postcode"
+          value={this.state.postcode}
+          onChange={this.handlePostcodeChange}
         />
         <input type="submit" value="Send" />
       </form>
