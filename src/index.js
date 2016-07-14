caee8605e25ba0e4dc50f024f922458ea8e2c5d6
Vincent Alcantara <vincent.alcantara@yahoo.com.au>
2016-07-14 19:25:22 +1000

    Added 'request' to package.json and an alert on submission.

--- src/index.js
+++ src/index.js
@@ -11,56 +11,58 @@ var AddressForm = React.createClass({
   handleAddressChange: function(e) {
     this.setState({address: e.target.value});
   },
-  getSuburbs: function(addrQueryStr) {
+  getSuburbs: function(e) {
+    // check if the suburb is at least 3 characters before calling api
+    // if no match set an error
+    var addrQueryStr = e.target.value;
     if (addrQueryStr.length > 2) {
       axios
-      .get(`http://localhost:3000/address?q=${addrQueryStr}`)
+      .get(`http://localhost:3000/address?q=${addrQueryStr}`) // we need to do this for CORS
       .then((resp) => {
         var localities = resp.data.localities.locality;
-        console.log('localities', localities);
-        var suburbs;
-        console.log('localities.length', localities.length);
-        if (localities.length > 0) {
-          suburbs = resp.data.localities.locality.map(function(elem){
+        if (localities && localities.length > 0) {
+          console.log('localities.length:', localities.length);
+          var suburbs = resp.data.localities.locality.map(function(elem){
             return elem.location + ', ' + elem.postcode + ', ' + elem.state;
           });
-        } else {
-          suburbs = {};
+          console.log('suburbs:', suburbs);
+          this.setState({suburbs: suburbs});
         }
-        console.log('suburbs', suburbs);
-        this.setState({suburbs: suburbs});
       })
     }
   },
   handleSuburbChange: function(e) {
-  	// check if the suburb is at least 3 characters before calling api
-  	// if no match set an error
-    this.getSuburbs(e.target.value);
-    this.setState({suburb: e.target.value});
+    // this.getSuburbs(e.target.value);
+    this.setState({suburb: e});
   },
   handleSubmit: function(e) {
     e.preventDefault();
     var address = this.state.address.trim();
     var suburb = this.state.suburb.trim();
-    // if (!suburb || !address) {
-    //   return;
-    // }
-    this.props.onAddressSubmit({address: address, suburb: suburb});
-    this.setState({address: '', suburb: ''});
+    if (!suburb || !address) {
+      return; // Exit if there's not a valid address or suburb
+    }
+    window.alert('We will send you your prize to:' + address + ', ' + suburb);
+    this.setState({address: '', suburb: '', suburbs: []});
   },
   render: function() {
     return (
       <form className="AddressForm" onSubmit={this.handleSubmit}>
+        <label>
+          Please enter your address:
+        </label>
         <input
           type="text"
-          placeholder="Enter address"
+          placeholder="Enter street address"
           value={this.state.address}
           onChange={this.handleAddressChange}
         />
         <Typeahead
-          onChange={this.handleSuburbChange}
+          onChange={this.getSuburbs}
           options={this.state.suburbs}
-          placeholder="Enter suburb"
+          placeholder="Enter suburb or postcode"
+          value={this.state.suburb}
+          onOptionSelected={this.handleSuburbChange}
         />
         <button type="submit">Send</button>
       </form>
